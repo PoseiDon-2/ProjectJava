8462f5e483440e2f4d7cdb0b43775641c6184fdb
misternotz <rilakuma129@hotmail.com>
2024-03-02 23:19:19 +0700

    ดึงข้อมูล

--- src/main/java/com/project/Controller/homeController.java
+++ src/main/java/com/project/Controller/homeController.java
@@ -1,10 +1,23 @@
 package com.project.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.project.model.diaryRepository;
+import com.project.model.diary;
+
 
 @Controller
 public class homeController {
+	
+
+	@Autowired
+	diaryRepository repof;
+
 
 	@GetMapping("/")
 	public String home() {
@@ -12,7 +25,11 @@ public class homeController {
 	}
 	
 	@GetMapping("/diary")
-	public String diary() {
+	public String diary(Model model) {
+
+
+		List<diary> d = repof.showall();
+		model.addAttribute("listDiary",d);
 		return "diary";
 	}
 	
