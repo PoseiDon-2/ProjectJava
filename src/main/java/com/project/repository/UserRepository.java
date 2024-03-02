599aaab14235b4730c1753ad7416073304597545
PoseiDon <suriya.b@kkumail.com>
2024-03-02 21:00:07 +0700

    asas

--- src/main/java/com/project/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.repository;
-
-import com.project.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-    User findByUsername(String username);
-}
