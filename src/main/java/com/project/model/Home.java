c4fd650292d19c8a4597ea2c33ebf3ad4f78963c
siwah <siwakorn.san@kkumail.com>
2024-03-02 21:25:08 +0700

    ใช่

--- src/main/java/com/project/model/Home.java
+++ src/main/java/com/project/model/Home.java
@@ -11,8 +11,37 @@ import jakarta.persistence.Id;
 public class Home {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String name;
+	private String massage;
+	private LocalDateTime post_data;
 	
-	private LocalDateTime post_data; 
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMassage() {
+		return massage;
+	}
+	public void setMassage(String massage) {
+		this.massage = massage;
+	}
+	public LocalDateTime getPost_data() {
+		return post_data;
+	}
+	public void setPost_data(LocalDateTime post_data) {
+		this.post_data = post_data;
+	}
+	
+	
+	
 
 }
